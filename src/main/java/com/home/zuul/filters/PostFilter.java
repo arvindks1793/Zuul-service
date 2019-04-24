ea650e35443ef3c13e9244db25527ba655d2e725
arvind <arvindkstechm@gmail.com>
2019-04-24 11:58:36 +0530

    initial commit zuul-service

---
+++ src/main/java/com/home/zuul/filters/PostFilter.java
@@ -0,0 +1,31 @@
+package com.home.zuul.filters;
+
+import com.netflix.zuul.ZuulFilter;
+
+public class PostFilter extends ZuulFilter {
+
+	@Override
+	public Object run() {
+		System.out.println("Using Post Filter");		
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "post";
+	}
+
+}
